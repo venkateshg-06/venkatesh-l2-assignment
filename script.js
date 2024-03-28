c1aa205747196941bb28cbbee48e999a90dea03b
unknown <venkateshgolla134@gmail.com>
2024-03-29 00:32:42 +0530

    fisrt

--- script.js
+++ script.js
@@ -2,47 +2,205 @@ var searchInput = document.getElementById("searchInput");
 var fisrtContainer = document.getElementById('firstData-container')
 var favContainer = document.getElementById("fav-container")
 var headingEle = document.querySelector(".heading-ele")
-var iconelement  = document.getElementById("icon-element")
 var sliderContainer = document.getElementById("sliderContainer")
+let splideListContainer = document.getElementById("splideListContainer")
+let favListContainer = document.getElementById("favContainer")
 
+
+function getFavListFromLocalStorage() {
+  let stringifiedFavList = localStorage.getItem("favList");
+  let parsedFavList = JSON.parse(stringifiedFavList);
+  if (parsedFavList === null) {
+      return [];
+  } else {
+      return parsedFavList;
+  }
+}
+
+
+
+let favList = getFavListFromLocalStorage();
+
+
+function FavsectionUpdate(){
+  displayFavSection(favList)
+
+}
+
+function removeItem(listId, each){
+  let eleEle = document.querySelector(`[data-id="${listId}"]`)
+  let index = favList.findIndex(eachEl => {
+    if (listId === eachEl.id)
+    {
+      return true
+    }
+    else {
+      return false
+    }
+  })
+  if (index === -1){
+    favList.push(each)
+    localStorage.setItem("favList", JSON.stringify(favList)) 
+   FavsectionUpdate()
+  }
+   splideListContainer.removeChild(eleEle)
+   
+
+   
+  
+}
+function displayFavSection(resultData) {
+ 
+
+  let favEl = document.getElementById("favEl");
+  
+   let r = resultData
+   
+   
+   r.forEach((each) => {
+     var listId = each.id 
+     let listEle = document.createElement("li")
+     listEle.setAttribute("data-id",each.id)
+     listEle.classList.add("splide__slide")
+
+
+   let itemsContainer = document.createElement("div")
+   itemsContainer.setAttribute("style", "width:100%;")
+   listEle.appendChild(itemsContainer)
+
+   let iconcontainer = document.createElement("div")
+   iconcontainer.classList.add("icon-bg")
+   itemsContainer.appendChild(iconcontainer)
+
+   iconcontainer.onclick = function(){
+     console.log(listId)
+     removeItem(listId, each)
+   }
+
+   let iconEle = document.createElement("i")
+   iconEle.classList.add("fa-solid","fa-heart", "red-icon" )  
+   iconcontainer.appendChild(iconEle)
+
+   let imgEle = document.createElement("img")
+   imgEle.src = each.src.medium 
+   imgEle.alt = "image"
+   imgEle.setAttribute("style", "width:100%; height:200px;")
+   itemsContainer.appendChild(imgEle)
+
+   let sliderName = document.createElement("p")
+   sliderName.classList.add("slider-image-name")
+   sliderName.textContent = each.alt
+   itemsContainer.appendChild(sliderName)
+
+   let photographer = document.createElement("p")
+   photographer.classList.add("slider-photographer")
+   photographer.textContent = each.photographer 
+   itemsContainer.appendChild(photographer)
+
+   favListContainer.appendChild(listEle)
+
+   })
+   
+
+   new Splide(favEl, {
+     type: 'slide',
+     perMove:1,
+     perPage: 3,
+     gap: "2rem",
+     pauseOnHover: false,
+     pagination: false,
+     arrows: true,
+     rewind: true,
+     breakpoints: {
+   640: {
+     perPage: 2,
+     gap    : '.7rem',
+     height : '300px',
+   },
+   480: {
+     perPage: 1,
+     gap    : '.7rem',
+     height : '300px',
+   },
+ },
+   }).mount();
+
+ }
         
 function createSlider(resultData) {
-  sliderContainer.textContent = "";
+   
+   let splideEl = document.getElementById("splideEl");
+
     let remainingData = resultData.photos.slice(1)
-    const newSliderContainer = document.createElement('div');
-    newSliderContainer.classList.add('slider-container');
-    const newSlider = document.createElement('div');
-    newSlider.classList.add('splide');
-     
-    const slideList = remainingData.map(each => `
-    <li class="splide__slide">
-    <div style="width:90%;" id=${each.id}">
-       <i class="fa-regular fa-heart icon" id="icon-element" ></i>
-       <img src="${each.src.small}" alt="Image" style="width:100%;height:200px;">
-      <p class="slider-image-name">${each.alt}</p>
-      <p class="slider-photographer">${each.photographer}</p>
-    </div>
-    </li>`).join('');
-    newSlider.innerHTML = `
-      <div class="splide__track">
-        <ul class="splide__list">
-          ${slideList}
-        </ul>
-      </div>
-    `;
+    console.log(remainingData)
+    
+    remainingData.forEach((each) => {
+      var listId = each.id 
+      let listEle = document.createElement("li")
+      listEle.setAttribute("data-id",each.id)
+      listEle.classList.add("splide__slide")
 
-    newSliderContainer.appendChild(newSlider);
-    sliderContainer.appendChild(newSliderContainer);
 
-    new Splide(newSlider, {
+    let itemsContainer = document.createElement("div")
+    itemsContainer.setAttribute("style", "width:100%;")
+    listEle.appendChild(itemsContainer)
+
+    let iconcontainer = document.createElement("div")
+    iconcontainer.classList.add("icon-bg")
+    itemsContainer.appendChild(iconcontainer)
+
+    iconcontainer.onclick = function(){
+      removeItem(listId, each)
+    }
+
+    let iconEle = document.createElement("i")
+    iconEle.classList.add("fa-regular","fa-heart", "icon" )
+    iconcontainer.appendChild(iconEle)
+
+    let imgEle = document.createElement("img")
+    imgEle.src = each.src.medium 
+    imgEle.alt = "image"
+    imgEle.setAttribute("style", "width:100%; height:200px;")
+    itemsContainer.appendChild(imgEle)
+
+    let sliderName = document.createElement("p")
+    sliderName.classList.add("slider-image-name")
+    sliderName.textContent = each.alt
+    itemsContainer.appendChild(sliderName)
+
+    let photographer = document.createElement("p")
+    photographer.classList.add("slider-photographer")
+    photographer.textContent = each.photographer 
+    itemsContainer.appendChild(photographer)
+
+    splideListContainer.appendChild(listEle)
+
+    })
+    
+
+    new Splide(splideEl, {
       type: 'slide',
       perMove:1,
       perPage: 3,
+      gap: "2rem",
       pauseOnHover: false,
       pagination: false,
       arrows: true,
       rewind: true,
+      breakpoints: {
+    640: {
+      perPage: 2,
+      gap    : '.7rem',
+      height : '300px',
+    },
+    480: {
+      perPage: 1,
+      gap    : '.7rem',
+      height : '300px',
+    },
+  },
     }).mount();
+
   }
 
   function displayFirstItem(result){
@@ -50,13 +208,13 @@ function createSlider(resultData) {
       console.log(firstData)
       fisrtContainer.innerHTML = `
               <div class="first-image-container">
-                  <div >
-                      <img src="${firstData.src.small}" class="fisrt-image"/>
+                  <div class="img-container-first">
+                      <img src="${firstData.src.medium}" class="fisrt-image"/>
                   </div>
                   <div class="first-content-container">
-                      <h3>${firstData.alt}</h3>
+                      <h3 class="first-item-heading">${firstData.alt}</h3>
                       <p class="author">${firstData.photographer}</p>
-                      <button class="naviagation-button"><a href="${firstData.photographer_url}" target="_blank">Explore More</a></button>
+                      <button class="naviagation-button"><a href="${firstData.photographer_url}" target="_blank">EXPLORE MORE</a></button>
                   </div>
               </div>`
   }
@@ -93,14 +251,15 @@ searchInput.addEventListener("keypress", function(event) {
              
              async function display(){
                  let result = await getResult()
-                 console.log(result)
+                 splideListContainer.textContent = ""
                  displayFirstItem(result)
                  createSlider(result)
                  let {photos} = result
                  console.log(photos)
              }
              display()
-            }
+            }   
         });
 
 
+displayFavSection(favList)
